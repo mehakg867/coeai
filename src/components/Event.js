5bd144ecb2399d2c9be272e6a14bdab8057b3d25
shrutidy16 <105289516+shrutidy16@users.noreply.github.com>
2022-07-11 00:44:34 +0530

    edited nav bar and home page

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 import { BrowserRouter as Router} from "react-router-dom";
-//import './App.css';
+import '../App.css';
 
 function Event() {
     return (
-        <div>
+        <div className='App'>
             <h1>Event page</h1>
+            <p>this is event page</p>
         </div>
     );
   }
