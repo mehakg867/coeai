18845ddbc077ba318a127741c13880e4a8c9c0bc
Mehakg867@ <mehakg867@gmail.com>
2022-07-17 12:38:59 +0530

    Home Nav

--- src/components/Home.js
+++ src/components/Home.js
@@ -32,34 +32,30 @@ function Home() {
       <div  className='bgimage'></div>
       <>
       <Navbar bg="light" varient= "bright" sticky="top" expand="lg">
-                <NavbarBrand>
-                    <img src={logo} width="40px" height="40px"/>
-                    INDIRA GANDHI DELHI TECHNICAL UNIVERSITY FOR WOMEN
-                </NavbarBrand>
-                <Nav>
-            
-                <Nav.Link as={Link} to="/Aboutus">About us</Nav.Link>
-                <Nav.Link as={Link} to="/People">People</Nav.Link>
+                
+                <Nav className='Navigation'>
+                <Nav.Link className='homenav' as={Link} to="/Aboutus">About us</Nav.Link>
+                <Nav.Link className='homenav'as={Link} to="/People">People</Nav.Link>
 
-                <NavDropdown title= "Research">
-                <NavDropdown.Item as={Link} to="/Research/Nlp">NLP</NavDropdown.Item>
-                <NavDropdown.Item  as={Link} to="/Research/ImageRecognition"> Image recognition</NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/Research/ComputerVision"> Computer Vision</NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/Research/SpeechTech"> Speech technology</NavDropdown.Item>
+                <NavDropdown className='homenav' title= "Research">
+                <NavDropdown.Item className='homenav'as={Link} to="/Research/Nlp">NLP</NavDropdown.Item>
+                <NavDropdown.Item className='homenav' as={Link} to="/Research/ImageRecognition"> Image recognition</NavDropdown.Item>
+                <NavDropdown.Item className='homenav'as={Link} to="/Research/ComputerVision"> Computer Vision</NavDropdown.Item>
+                <NavDropdown.Item className='homenav'as={Link} to="/Research/SpeechTech"> Speech technology</NavDropdown.Item>
                 </NavDropdown> 
 
-                <Nav.Link as={Link} to="/Event">Event</Nav.Link>
+                <Nav.Link className='homenav'as={Link} to="/Event">Event</Nav.Link>
 
-                <NavDropdown title= "Outreach Program">
-                <NavDropdown.Item as={Link} to="/Outreach/Projects">Projects</NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/Outreach/Products"> Products</NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/Outreach/Patents"> Patents</NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/Outreach/Publications"> Publications</NavDropdown.Item>
+                <NavDropdown className='homenav'title= "Outreach Program">
+                <NavDropdown.Item className='homenav'as={Link} to="/Outreach/Projects">Projects</NavDropdown.Item>
+                <NavDropdown.Item className='homenav'as={Link} to="/Outreach/Products"> Products</NavDropdown.Item>
+                <NavDropdown.Item className='homenav'as={Link} to="/Outreach/Patents"> Patents</NavDropdown.Item>
+                <NavDropdown.Item className='homenav'as={Link} to="/Outreach/Publications"> Publications</NavDropdown.Item>
                 </NavDropdown> 
 
-                <Nav.Link as={Link} to="/Conference">Conference</Nav.Link>
-                <Nav.Link as={Link} to="/Joinus">Joinus</Nav.Link>
-                <Nav.Link as={Link} to="/Partnership">Partnership</Nav.Link>
+                <Nav.Link className='homenav' as={Link} to="/Conference">Conference</Nav.Link>
+                <Nav.Link className='homenav'as={Link} to="/Joinus">Joinus</Nav.Link>
+                <Nav.Link className='homenav'as={Link} to="/Partnership">Partnership</Nav.Link>
 
         </Nav>
             </Navbar>
--- src/container/Navigation.jsx
+++ src/container/Navigation.jsx
@@ -22,30 +22,30 @@ function Navigation(){
                       <img src={logo} width="40px" height="40px"/>
                       INDIRA GANDHI DELHI TECHNICAL UNIVERSITY FOR WOMEN
                   </NavbarBrand>
-                  <Nav>
+                  <Nav className='pagenav'>
               
-                  <Nav.Link as={Link} to="/Aboutus">About us</Nav.Link>
-                  <Nav.Link as={Link} to="/People">People</Nav.Link>
+                  <Nav.Link className='allnav' as={Link} to="/Aboutus">About us</Nav.Link>
+                  <Nav.Link className='allnav'as={Link} to="/People">People</Nav.Link>
   
-                  <NavDropdown title= "Research">
-                  <NavDropdown.Item as={Link} to="/Research/Nlp">NLP</NavDropdown.Item>
-                  <NavDropdown.Item  as={Link} to="/Research/ImageRecognition"> Image recognition</NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to="/Research/ComputerVision"> Computer Vision</NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to="/Research/SpeechTech"> Speech technology</NavDropdown.Item>
+                  <NavDropdown className='allnav' title= "Research">
+                  <NavDropdown.Item className='allnav'as={Link} to="/Research/Nlp">NLP</NavDropdown.Item>
+                  <NavDropdown.Item className='allnav' as={Link} to="/Research/ImageRecognition"> Image recognition</NavDropdown.Item>
+                  <NavDropdown.Item className='allnav'as={Link} to="/Research/ComputerVision"> Computer Vision</NavDropdown.Item>
+                  <NavDropdown.Item className='allnav'as={Link} to="/Research/SpeechTech"> Speech technology</NavDropdown.Item>
                   </NavDropdown> 
   
-                  <Nav.Link as={Link} to="/Event">Event</Nav.Link>
+                  <Nav.Link className='allnav'as={Link} to="/Event">Event</Nav.Link>
   
-                  <NavDropdown title= "Outreach Program">
-                  <NavDropdown.Item as={Link} to="/Outreach/Projects">Projects</NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to="/Outreach/Products"> Products</NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to="/Outreach/Patents"> Patents</NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to="/Outreach/Publications"> Publications</NavDropdown.Item>
+                  <NavDropdown className='allnav'title= "Outreach Program">
+                  <NavDropdown.Item className='allnav'as={Link} to="/Outreach/Projects">Projects</NavDropdown.Item>
+                  <NavDropdown.Item className='allnav'as={Link} to="/Outreach/Products"> Products</NavDropdown.Item>
+                  <NavDropdown.Item className='allnav'as={Link} to="/Outreach/Patents"> Patents</NavDropdown.Item>
+                  <NavDropdown.Item className='allnav'as={Link} to="/Outreach/Publications"> Publications</NavDropdown.Item>
                   </NavDropdown> 
   
-                  <Nav.Link as={Link} to="/Conference">Conference</Nav.Link>
-                  <Nav.Link as={Link} to="/Joinus">Joinus</Nav.Link>
-                  <Nav.Link as={Link} to="/Partnership">Partnership</Nav.Link>
+                  <Nav.Link className='allnav'as={Link} to="/Conference">Conference</Nav.Link>
+                  <Nav.Link className='allnav'as={Link} to="/Joinus">Joinus</Nav.Link>
+                  <Nav.Link className='allnav'as={Link} to="/Partnership">Partnership</Nav.Link>
   
           </Nav>
               </Navbar>
