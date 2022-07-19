5e26a01411d7f3953efbd245b9b9de20ae9ab17a
shrutidy16 <105289516+shrutidy16@users.noreply.github.com>
2022-07-19 23:34:12 +0530

    integrated with firebase

--- src/components/Publications.js
+++ src/components/Publications.js
@@ -1,14 +1,38 @@
-import React, {Component} from 'react';
-import { BrowserRouter as Router} from "react-router-dom";
+import React, {useEffect, useState} from 'react';
 import Navigation from '../container/Navigation';
+import { getFirestore } from "firebase/firestore";
+import { collection, getDocs } from 'firebase/firestore';
+import app from '../firebase';
+const db = getFirestore(app);
 
-function Publications() {
+ function Publications() {
+      console.log("start");
+      const [dataToShow, setData] = useState([]);
+        useEffect(() => {
+      
+      const data = [];
+          const getValues = async () => {
+            const querySnapshot = await getDocs(collection(db, "publications"));
+            querySnapshot.forEach((doc) => {
+                data.push(doc.data().info)
+            });
+               setData(data);
+          }
+       getValues();
+    },[]);
+   
     return (
         <div>
         <Navigation/>
             <h1>Publication page</h1>
+            {dataToShow.map((item) => (
+                
+                <div className='pubItems' key={item.id}>
+                <p>{item}</p>
+                </div>
+                ))}
         </div>
-    );
+);
   }
   
   export default Publications;
