ee05e4b323ff05676142d32adc9048d15d47ea19
Mehakg867@ <mehakg867@gmail.com>
2022-07-22 12:40:28 +0530

    Merge branch 'Shruti' of https://github.com/mehakg867/coeai into mehak-branch

--- src/components/Conference.js
+++ src/components/Conference.js
@@ -29,13 +29,13 @@ function Conference() {
                 <div className="col px-0">
                   <Row>
                     <Col lg="10">
-                      <h1 className="display-3 text-white">
+                      <h1 className="display-3 text-dark">
                        About Conference
                       </h1>
-                      <p className="lead text-white">
+                      <p className="lead text-dark">
                       After the grand success of three versions of International Conference on Artificial Intelligence and Speech Technology (AIST2019, AIST2020 and AIST2021), Indira Gandhi Delhi Technical University for Women, Delhi is all set to ready for hosting its fourth version i.e. 4th International Conference on Artificial Intelligence and Speech Technology (AIST2022) on 9-10th December, 2022 at IGDTUW Campus, Delhi. Last three Conferences witnessed a number of Expert Researchers, Academicians and Industry Practioners in AI and Speech Domain from premier Institutions in India and abroad including Japan, Australia, France, Germany,  Hungary, Czech Republic, Myanmar, Uzbekistan, Austria, Vietnam and others.
                     </p>
-                    <p className="lead text-white">
+                    <p className="lead text-dark">
                       Thanks to Artificial Intelligence (AI), Machine Learning(ML), Natural Language Processing (NLP) and Speech Technology, which are enabling the development of Virtual Assistants in user-facing applications and working in the background to automate workflows. AI driven solutions like speech recognition systems are more accurate and efficient and makes it easier to identify and understand the components of natural language. These models can be used for voice recognition services, making an NLP service well-rounded and more efficient for various domain including medical (e.g. speech impaired people), forensics and academics and others.
                       </p>
                       <div className="btn-wrapper">
@@ -71,7 +71,7 @@ function Conference() {
               
             </div>
             <Wave style={{ transform: 'rotate(180deg)' }}
-            fill='rgb(103, 157, 245)'
+            fill='#DBDFFD'
             paused={false}
             options={{
               height: 80,
