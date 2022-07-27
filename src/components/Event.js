44a204f4cad32584c0955ca70c6518273366a166
shrutidy16 <105289516+shrutidy16@users.noreply.github.com>
2022-07-27 12:52:54 +0530

    added cards to event component

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,14 +1,61 @@
-import React, {Component} from 'react';
+import React, {useEffect, useState} from 'react';
 import { BrowserRouter as Router} from "react-router-dom";
 import Navigation from '../container/Navigation';
 import '../App.css';
+import { getFirestore } from "firebase/firestore";
+import { collection, getDocs } from 'firebase/firestore';
+import app from '../firebase';
+import './Event.css';
+
+
+const db = getFirestore(app);
 
 function Event() {
+
+    console.log("start");
+    const[dataToShow, setData] = useState([]);
+    
+
+
+      useEffect(() => {
+    const data= [];
+    
+        const getValues = async () => {
+          const querySnapshot = await getDocs(collection(db, "Events"));
+          querySnapshot.forEach((doc) => {
+            data.push(doc.data());
+             setData(data);
+          });
+             
+        }
+     getValues();
+  },[]);
+
     return (
         <div>
         <Navigation />
-            <h1>Event page</h1>
-            <p>this is event page</p>
+            
+
+{dataToShow.map((item) => (
+    <div className='expert'>
+<div className='expertTalks' key={item.Id}>
+    <div className='expertImg'>
+        <img src="https://th.bing.com/th/id/OIP.X4WLXoJLdIgQu5LO5UxIoQAAAA?w=171&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7" alt=''/>
+        <p>{item.name}</p>
+        <p>{item.designation}</p>
+    </div>
+    <div className='expertTopic'>
+    <div className='expertDate'>
+        {item.date}
+    </div>
+        <h1 className='expertTopic'>{item.topic}</h1>
+        <p className='expertText'>{item.info}</p>
+    </div>
+</div>
+</div>
+                ))}
+
+
         </div>
     );
   }
