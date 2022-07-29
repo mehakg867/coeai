2ad7762e36fad92acba36c1360d16b75577a540c
shrutidy16 <105289516+shrutidy16@users.noreply.github.com>
2022-07-29 14:35:11 +0530

    added patent page

--- src/components/Pat.js
+++ src/components/Pat.js
@@ -1,14 +1,44 @@
-import React, {Component} from 'react';
-import { BrowserRouter as Router} from "react-router-dom";
+import React, {useEffect, useState} from 'react';
 import Navigation from '../container/Navigation';
-import '../App.css';
+import { getFirestore } from "firebase/firestore";
+import { collection, getDocs } from 'firebase/firestore';
+import app from '../firebase';
+const db = getFirestore(app);
 
 function Pat() {
+    const[dataToShow, setData] = useState([]);
+    useEffect(() => {
+  const data= [];
+  
+      const getValues = async () => {
+        const querySnapshot = await getDocs(collection(db, "Patents"));
+        querySnapshot.forEach((doc) => {
+          data.push(doc.data());
+           setData(data);
+        });
+           
+      }
+   getValues();
+},[]);
     return (
-        <div>
+
+        <div className='bodyback'>
+        
         <Navigation />
-            <h1>Patent page</h1>
-            <p>this is Patents page</p>
+            <h1>Patents</h1>
+            {dataToShow.map((item) => (
+                <div className='expert'>
+<div className='expertTalks' key={item.Id}>
+    <div>
+        <h1 className='expertTopic' style={{fontSize:'1.7vw', fontWeight:'bolder', paddingRight:'2vw'}}> {item.author}</h1>
+        <p className='expertText' style={{fontSize:'1.5vw',fontWeight:'bold', paddingRight:'2vw'}}> {item.title}</p>
+        <p className='expertText' style={{fontSize:'1.3vw',fontWeight:'normal', paddingRight:'2vw'}}> {item.content1}</p>
+        <p className='expertText' style={{fontSize:'1.3vw',fontWeight:'normal', paddingRight:'2vw'}}> {item.content2}</p>
+
+    </div>
+</div>
+</div>
+            ))}
         </div>
     );
   }
