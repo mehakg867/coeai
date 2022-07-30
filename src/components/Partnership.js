b5efd1549ae3a8d3818654bbc7c487464a62490f
shrutidy16 <105289516+shrutidy16@users.noreply.github.com>
2022-07-30 17:00:40 +0530

    changing in pat.js

--- src/components/Partnership.js
+++ src/components/Partnership.js
@@ -2,12 +2,14 @@ import React, {Component} from 'react';
 import { BrowserRouter as Router} from "react-router-dom";
 import '../App.css';
 import Navigation from '../container/Navigation';
+import construction from "../images/construction.jpg";
 
 function Partnership() {
     return (
         <div>
         <Navigation />
             <h1>Partnership page</h1>
+            <img src={construction}/>
         </div>
     );
   }
