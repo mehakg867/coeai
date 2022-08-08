58492a145bceccc85ddf4dd4a34cf86991899490
shrutidy16 <105289516+shrutidy16@users.noreply.github.com>
2022-08-09 01:17:04 +0530

    adding components for admin

---
+++ src/admin/Admin.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import { BrowserRouter as Router} from "react-router-dom";
+import '../App.css';
+import Navigation from '../container/Navigation';
+import construction from "../images/construction.jpg";
+
+
+
+function Admin() {
+    return (
+        <div>
+        <Navigation />
+            <h1>Partnership page</h1>
+            <img src={construction}/>
+        </div>
+    );
+  }
+  
+  export default Admin;
