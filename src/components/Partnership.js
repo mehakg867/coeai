040fe6c6168d623cdaf4c7909a1e299c49ff76c8
Mehakg867@ <mehakg867@gmail.com>
2022-08-20 18:23:19 +0530

    mous added

--- src/components/Partnership.js
+++ src/components/Partnership.js
@@ -12,7 +12,7 @@ function Partnership() {
             <h1 className='pat'>Partnership page</h1>
             <h2 style={{fontFamily: "'Kaisei Opti', serif",fontSize:"2.5vw"}}>Government bodies/ Institutes/ Organizations
 </h2>
-            <Marquee gradient="false" speed={30}>
+            <Marquee gradient="false" gradientWidth={10} speed={50}>
             <img className="gbrand"src="https://firebasestorage.googleapis.com/v0/b/coeai-fcece.appspot.com/o/govt_brands%2F780.png?alt=media&token=cfb8bc03-facc-4e06-ba65-02937ccdb6c2" alt=""></img>
             <img className="gbrand"src="https://firebasestorage.googleapis.com/v0/b/coeai-fcece.appspot.com/o/govt_brands%2FDSSSB-Logo-3.jpg?alt=media&token=cda5d70d-677f-4383-9c1a-20cddf67be80" alt=""></img>
             <img className="gbrand"src="https://firebasestorage.googleapis.com/v0/b/coeai-fcece.appspot.com/o/govt_brands%2FMCD_Logo.png?alt=media&token=da975085-4f4a-4c10-8826-d8f907f3dc68" alt=""></img>
@@ -22,9 +22,23 @@ function Partnership() {
             <img className="gbrand"src="https://firebasestorage.googleapis.com/v0/b/coeai-fcece.appspot.com/o/govt_brands%2Fipgcl-logo.png?alt=media&token=758ae125-1479-4955-a21a-b674ede6cf05" alt=""></img>
             <img className="gbrand"src="https://firebasestorage.googleapis.com/v0/b/coeai-fcece.appspot.com/o/govt_brands%2Fpost.png?alt=media&token=77d3a084-56e5-4a87-8145-22ea75eb0dfa" alt=""></img>
   </Marquee>
+  <h2 style={{fontFamily: "'Kaisei Opti', serif" ,marginTop:"2vw",fontSize:"2.5vw"}}>INTERNATIONAL MOU’S</h2>
+  <Marquee gradient="false" gradientWidth={10}  speed={50}>
+            <img className="brand"src="https://firebasestorage.googleapis.com/v0/b/coeai-fcece.appspot.com/o/govt_brands%2F18.unimap-logo-removebg-preview.png?alt=media&token=87ec7c4e-099c-4cef-affe-5a38253f5144"  alt="unimap" style={{height:"20vw",width:"20vw"}}></img>
+            <img className="brand"src="https://firebasestorage.googleapis.com/v0/b/coeai-fcece.appspot.com/o/govt_brands%2Flogo.png?alt=media&token=a7ff6abf-f134-464c-a77f-f3fe10007fd7" alt="rekhi" style={{width:"20vw" ,height:"10vw"}}></img>
+            <img className="brand"src="https://firebasestorage.googleapis.com/v0/b/coeai-fcece.appspot.com/o/govt_brands%2Fnexus-logo-headline-with-subline.png?alt=media&token=0fda1fe8-83c4-4f6f-b908-ae84e3794081" alt="nexus" style={{width:"25vw"}}></img>
+            <img className="brand"src="https://firebasestorage.googleapis.com/v0/b/coeai-fcece.appspot.com/o/govt_brands%2Fuk-ceh-new-logo-removebg-preview.png?alt=media&token=60776c86-35db-42b2-8ec1-fa7969a93ea3" alt="ukceh" style={{width:"19vw",height:"13vw"}}></img>
+            <img className="brand"src="https://firebasestorage.googleapis.com/v0/b/coeai-fcece.appspot.com/o/govt_brands%2F890237-middle-removebg-preview.png?alt=media&token=5f079833-c1e8-4297-93e6-de953058ca68" alt="nara" style={{height:"15vw",width:"13vw"}}></img>
+            <img className="brand"src="https://firebasestorage.googleapis.com/v0/b/coeai-fcece.appspot.com/o/govt_brands%2FShraman_logo-removebg-preview.png?alt=media&token=301650af-b8e7-4973-b3a2-fc0157e8e99f" alt="shraman" style={{height:"10vw",width:"15vw"}}></img>
+            <img className="brand"src="https://firebasestorage.googleapis.com/v0/b/coeai-fcece.appspot.com/o/govt_brands%2F11786-removebg-preview.png?alt=media&token=b3f85712-2646-44c4-8973-7473dc2209cb" alt="mykop russia" style={{width:"15vw"}}></img>
+            <img className="brand"src="https://firebasestorage.googleapis.com/v0/b/coeai-fcece.appspot.com/o/govt_brands%2F%E5%AD%B8%E6%A0%A1logo1-0227-e1628652241130-removebg-preview.png?alt=media&token=4b51f50e-ffa7-4f69-92ac-5818d668f227" alt="npst"></img>
+            <img className="brand"src="https://firebasestorage.googleapis.com/v0/b/coeai-fcece.appspot.com/o/govt_brands%2Fpng-clipart-deutsche-gesellschaft-fur-internationale-zusammenarbeit-federal-ministry-of-economic-cooperation-and-development-germany-sustainable-development-cooperation-internationale-giz-text-logo-re.png?alt=media&token=bf42edcd-5639-485e-b948-70757122cc8c" alt="deutche" style={{height:"20vw",width:"20vw"}} ></img>
+            <img className="brand"src="https://firebasestorage.googleapis.com/v0/b/coeai-fcece.appspot.com/o/govt_brands%2Fpng-transparent-james-cook-university-singapore-cairns-organization-school-school-blue-text-logo-removebg-preview.png?alt=media&token=344f1a66-4857-4be0-8a7b-86f30d394a78" alt="james cook" style={{width:"18vw"}}></img>
+         
+  </Marquee>
   <h2 style={{fontFamily: "'Kaisei Opti', serif" ,marginTop:"2vw",fontSize:"2.5vw"}}>INDUSTRIAL ORGANIZATIONS
 </h2>
-<Marquee gradient="false" speed={50}>
+<Marquee gradient="false" gradientWidth={10}  speed={50}>
             <img className="brand"src="https://firebasestorage.googleapis.com/v0/b/coeai-fcece.appspot.com/o/govt_brands%2F1646199475329-removebg-preview.png?alt=media&token=7fe9f65a-b502-45c3-af2c-c4398bdeb3dc"  alt="edunet"></img>
             <img className="brand"src="https://firebasestorage.googleapis.com/v0/b/coeai-fcece.appspot.com/o/govt_brands%2FNASSCOM-Background-Check-Solutions-removebg-preview.png?alt=media&token=591aaa2c-d9a2-4321-8608-6004b1a26639" alt="nasscom"></img>
             <img className="brand"src="https://firebasestorage.googleapis.com/v0/b/coeai-fcece.appspot.com/o/govt_brands%2Fennoble-ip-logo.png?alt=media&token=430a91ac-c039-4aa2-a956-4fa33ec3873d" alt="enlobeip"></img>
